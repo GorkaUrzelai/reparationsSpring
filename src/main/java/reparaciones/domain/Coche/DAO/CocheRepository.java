216458f8c789c6b7405e3fd0092e7d105bf7cfbd
GorkaUrzelai <gorka.urzelai@alumni.mondragon.edu>
2022-02-08 23:57:29 +0100

    update de kilometros coche

--- src/main/java/reparaciones/domain/Coche/DAO/CocheRepository.java
+++ src/main/java/reparaciones/domain/Coche/DAO/CocheRepository.java
@@ -1,8 +1,10 @@
 package reparaciones.domain.Coche.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import reparaciones.domain.Coche.Model.Coche;
 
 import java.util.List;
@@ -15,4 +17,8 @@ public interface CocheRepository extends JpaRepository<Coche, Integer> {
     @Query("select co from Coche co join Customer cu on cu.id = co.dueño where cu.id=?1")
     List<Coche> getCoches(int customerId);
 
+    @Transactional
+    @Modifying
+    @Query("update Coche c set c.kilometros=?2 where c.cocheID =?1")
+    void editCocheKilometros(Integer c, Integer kilometros);
 }
